e14edc5e05b57b7e37273230893b3518c14cc1b7
gertminov <jacobheim@web.de>
2024-04-14 18:56:44 +0200

    added missing files

---
+++ src/routes/about/BgImage.svelte
@@ -0,0 +1,3 @@
+<div class="absolute">
+	<img src="/imgs/katrin-bg.png" alt="" class="h-screen w-screen object-cover" />
+</div>
---
+++ src/routes/about/CVDisplay.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Wrapper from '$lib/components/Wrapper.svelte';
+	import SkribbleButton from '$lib/components/SkribbleButton.svelte';
+	import SectionHeader from '$lib/components/SectionHeader.svelte';
+</script>
+
+<Wrapper>
+	<SectionHeader>My CV</SectionHeader>
+	<div class=" grid grid-cols-1 gap-8 lg:grid-cols-2">
+		<img src="/cv/cv-pic-1.png" alt="" class="" />
+		<img src="/cv/cv-pic-2.png" alt="" class="" />
+	</div>
+	<div>
+		<SkribbleButton>Download my resume</SkribbleButton>
+	</div>
+</Wrapper>
