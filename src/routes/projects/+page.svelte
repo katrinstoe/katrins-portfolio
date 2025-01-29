2f5802703b273a8ff18cd317aba61f3503f57b50
Katrin Franziska Hanna Stötter <stotter@kth.se>
2025-01-29 22:15:49 +0100

    fixed issue with mobile project overview

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,22 +1,19 @@
 <script>
-	import IconGithub from '$lib/components/icons/IconGithub.svelte';
 	import SectionHeader from '$lib/components/SectionHeader.svelte';
-	import ContactHeroShapes from '$lib/components/shapes/ContactHeroShapes.svelte';
-	import IconLinkedIn from '$lib/components/icons/IconLinkedIn.svelte';
-	import Shapes from '$lib/components/Shapes.svelte';
 	import HeroShapes from '$lib/components/shapes/HeroShapes.svelte';
-	import Introduction from '$lib/sections/hero/Introduction.svelte';
 	import ProjectCategories from '$lib/sections/projectCategories/ProjectCategories.svelte';
 
 	let projectIntro =
 		'The project page contains a collection of projects that I have completed during my studies at university or during internships at a company. These projects cover a wide range of fields, including UX research, programming, and design. In each section, you can find examples of my work and see how I have applied my skills and knowledge to tackle a variety of challenges. Be sure to check them out!';
 </script>
 
-<div class="relative min-h-screen">
+<div class="relative min-h-screen overflow-hidden pt-20 sm:pt-32">
 	<HeroShapes />
-	<div class="absolute grid h-full w-full place-content-center">
+	<div class="relative z-10 flex items-center justify-center p-4">
 		<div class="wrapper max-w-4xl">
-			<div class="basis-96 rounded-lg bg-white bg-opacity-60 xl:bg-transparent">
+			<div
+				class="basis-96 rounded-lg bg-white bg-opacity-90 p-6 sm:bg-opacity-60 xl:bg-transparent"
+			>
 				<SectionHeader>Projects</SectionHeader>
 				<p class="text-xl">{projectIntro}</p>
 
