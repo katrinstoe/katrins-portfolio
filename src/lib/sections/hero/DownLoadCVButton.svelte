23be57478eeabe61e01534add4033f75830cb103
Katrin Franziska Hanna Stötter <stotter@kth.se>
2025-01-30 11:20:42 +0100

    added new CV

--- src/lib/sections/hero/DownLoadCVButton.svelte
+++ src/lib/sections/hero/DownLoadCVButton.svelte
@@ -5,13 +5,14 @@
 	onMount(() => {
 		isGerman = navigator.language.startsWith('de-');
 	});
-	$: pdfLink = isGerman ? 'cv/de-cv-long-ks.pdf' : 'cv/en-cv-long-ks.pdf';
+	//$: pdfLink = isGerman ? 'cv/de-cv-long-ks.pdf' : 'cv/en-cv-long-ks-new.pdf';
+	$: pdfLink = 'cv/en-cv-long-ks-new.pdf';
 </script>
 
 <div class=" mt-16 flex h-12 flex-col justify-center">
 	<div class="relative h-12 transition-all duration-300 hover:rotate-2 hover:scale-105">
 		<img class="absolute bottom-0" src="/shapes/call-to-action.svg" alt="Katrin portrait" />
-		<div class=" sm:left-12 absolute bottom-3 left-10 w-full">
+		<div class=" absolute bottom-3 left-10 w-full sm:left-12">
 			<a class="text-md mx-auto xs:text-xl" href={pdfLink} target="_blank" download={pdfLink}
 				>Download my resume
 			</a>
--- src/routes/about/CVDisplay.svelte
+++ src/routes/about/CVDisplay.svelte
@@ -6,10 +6,9 @@
 
 <Wrapper>
 	<SectionHeader>My CV</SectionHeader>
-	<!-- TODO: Update CV -->
 	<div class=" grid grid-cols-1 gap-8 lg:grid-cols-2">
-		<img src="/cv/cv-pic-1.png" alt="" class="" />
-		<img src="/cv/cv-pic-2.png" alt="" class="" />
+		<img src="/cv/cv_katrin_p1.png" alt="CV Page 1" class="" />
+		<img src="/cv/cv_katrin_p2.png" alt="CV Page 2" class="" />
 	</div>
 	<div>
 		<SkribbleButton>Download my resume</SkribbleButton>
