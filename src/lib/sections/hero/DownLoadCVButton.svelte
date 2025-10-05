da7d857a6be191a216ea77603638f623a9c59bfd
Katrin Franziska Hanna Stötter <stotter@kth.se>
2025-10-05 15:27:01 +0200

    include new CV

--- src/lib/sections/hero/DownLoadCVButton.svelte
+++ src/lib/sections/hero/DownLoadCVButton.svelte
@@ -6,20 +6,22 @@
 		isGerman = navigator.language.startsWith('de-');
 	});
 	//$: pdfLink = isGerman ? 'cv/de-cv-long-ks.pdf' : 'cv/en-cv-long-ks-new.pdf';
-	$: pdfLink = 'cv/CV_Katrin_Stoetter_2025.pdf';
+	// $: pdfLink = 'cv/CV_Katrin_Stoetter_2025.pdf';
+	export let pdfLink: string;
+	export let buttonText: string;
 </script>
 
 <div class=" mt-16 flex h-12 flex-col justify-center">
 	<div class="relative h-12 transition-all duration-300 hover:rotate-2 hover:scale-105">
 		<img class="absolute bottom-0" src="/shapes/call-to-action.svg" alt="Katrin portrait" />
-		<div class=" absolute bottom-3 left-10 w-full sm:left-12">
+		<div class="absolute bottom-3 left-10 w-full sm:left-7">
 			<a
 				class="text-md mx-auto xs:text-xl"
 				href={pdfLink}
 				target="_blank"
 				download={pdfLink}
 				aria-label="Downloads Resume as PDF externally"
-				>Download my resume
+				>{buttonText}
 			</a>
 		</div>
 	</div>
--- src/routes/about/CVDisplay.svelte
+++ src/routes/about/CVDisplay.svelte
@@ -2,16 +2,54 @@
 	import Wrapper from '$lib/components/Wrapper.svelte';
 	import SectionHeader from '$lib/components/SectionHeader.svelte';
 	import DownLoadCVButton from '$lib/sections/hero/DownLoadCVButton.svelte';
+	import SubsectionHeader from '$lib/components/SubsectionHeader.svelte';
 	export let title = 'CV Page';
+	let academicPdf = '/cv/CVKatrinStötter_academia.pdf';
+	let industryPdf = '/cv/CVKatrinStötter_industry.pdf';
 </script>
 
 <Wrapper>
 	<SectionHeader>My CV</SectionHeader>
-	<div class=" grid grid-cols-1 gap-8 lg:grid-cols-2">
-		<img src="/cv/cv_katrin_p1.png" alt="CV Page 1" class="" />
-		<img src="/cv/cv_katrin_p2.png" alt="CV Page 2" class="" />
-	</div>
-	<div>
-		<DownLoadCVButton>Download my resume</DownLoadCVButton>
+
+	<div class="grid grid-cols-1 gap-8 lg:grid-cols-2">
+		<!-- Academic Roles -->
+		<div class="flex flex-col gap-4">
+			<SubsectionHeader>Academic Roles</SubsectionHeader>
+
+			<div class="relative min-h-[600px] flex-1 overflow-hidden rounded-xl shadow-lg">
+				<iframe
+					src="/cv/CVKatrinStötter_academia.pdf"
+					class="h-full w-full border-0"
+					title="Academic CV"
+				></iframe>
+			</div>
+
+			<DownLoadCVButton
+				pdfLink={'/cv/CVKatrinStötter_academia.pdf'}
+				buttonText="Download Academic CV"
+			>
+				Download my Academic CV
+			</DownLoadCVButton>
+		</div>
+
+		<!-- Industry Roles -->
+		<div class="flex flex-col gap-4">
+			<SubsectionHeader>Industry Roles</SubsectionHeader>
+
+			<div class="relative min-h-[600px] flex-1 overflow-hidden rounded-xl shadow-lg">
+				<iframe
+					src="/cv/CVKatrinStötter_industry.pdf"
+					class="h-full w-full border-0"
+					title="Industry CV"
+				></iframe>
+			</div>
+
+			<DownLoadCVButton
+				pdfLink={'/cv/CVKatrinStötter_industry.pdf'}
+				buttonText="Download Industry CV"
+			>
+				Download Industry CV
+			</DownLoadCVButton>
+		</div>
 	</div>
 </Wrapper>
